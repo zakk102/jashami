ab93912c69bae8ad5c69148c1f9a4fd385e1cb86
何育誠 <zakk@nanisodamakotos-MacBook-Pro.local>
2012-03-10 22:58:08 +0800

    init project

---
+++ js/router.js
@@ -0,0 +1,42 @@
+// Filename: router.js
+(function(mainHomeView){
+	var AppRouter = Backbone.Router.extend({
+		routes: {
+			// Define some URL routes
+			'startPage/:tab': 'startPage',
+			// Default
+			'*actions': 'defaultAction'
+		},
+		initialize:function () {
+	        $('.back').live('click', function(event) {
+	            window.history.back();
+	            return false;
+	        });
+	        this.firstPage = true;
+	
+	    },
+		defaultAction: function(actions){
+			startPage('orderTab');
+		},
+		startPage: function(tab){
+			var uuu = new mainHomeView();
+			uuu.render(tab);
+			$('#mainRoot').html(uuu.el);
+			EasyScroller.init();
+		},
+		changePage:function (page) {
+	        $(page.el).attr('data-role', 'page');
+	        page.render();
+	        $('body').append($(page.el));
+	        var transition = $.mobile.defaultPageTransition;
+	        // We don't want to slide the first page
+	        if (this.firstPage) {
+	            transition = 'none';
+	            this.firstPage = false;
+	        }
+	        $.mobile.changePage($(page.el), {changeHash:false, transition: transition});
+	    }
+	});
+	
+	window.myapp.Router = AppRouter;
+})(window.myapp.MainHomeView);
