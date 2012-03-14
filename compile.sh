b8449c5077acb968b9fe48d4a9369cb59139d49e
何育誠 <zakk@o2oist.com>
2012-03-15 01:50:57 +0800

    re-organize js folder

--- compile.sh
+++ compile.sh
@@ -7,12 +7,12 @@ java -jar ../compiler.jar --compilation_level=SIMPLE_OPTIMIZATIONS --charset=UTF
 ./js/i18n/zh-tw.js \
 ./js/main.js \
 ./js/images.js \
-./js/views/widget/scroller.js \
-./js/views/widget/touchWidget.js \
-./js/views/pages/orderTab.js \
-./js/views/pages/historyTab.js \
-./js/views/pages/feedbackTab.js \
-./js/views/pages/aboutUsTab.js \
-./js/views/pages/startPage.js \
+./js/widget/scroller.js \
+./js/widget/touchWidget.js \
+./js/pages/orderTab.js \
+./js/pages/historyTab.js \
+./js/pages/feedbackTab.js \
+./js/pages/aboutUsTab.js \
+./js/pages/startPage.js \
 ./js/router.js \
 --js_output_file=complied.js
