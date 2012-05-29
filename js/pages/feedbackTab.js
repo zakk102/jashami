ce050203e5a78a10986694c856d5aacd8d8015b9
何育誠 <zakk@o2oist.com>
2012-05-29 19:45:42 +0800

    change feedback

--- js/pages/feedbackTab.js
+++ js/pages/feedbackTab.js
@@ -1,11 +1,11 @@
 // Filename: js/pages/feedbackTab.js
-(function(Scroller, FeedbackServiceUrl, LocalModel){
+(function(LocalModel, Scroller, FeedbackServiceUrl, LocalModel){
 	var tabTemplate = [
 		"<p class='about_text' >",
 			"我的手機是",
-			"<input id='cellphone' class='INPUT Text' value='<%= cellphone %>'><br/><br/>",
+			"<input id='cellphone' class='INPUT Tel' type='Tel'><br/><br/>",
 			"甲蝦米有這些問題：<br/>",
-			"<textarea id='opinion' type='Text' class='INPUT Text'></textarea><br/>",
+			"<textarea id='errorReport' type='Text' class='INPUT Text'></textarea><br/>",
 			"我想要這些店家加入甲蝦米：<br/>",
 			"<input id='recommendation' type='Text' class='INPUT Text'><br/><br/>",
 		"</p>",
@@ -14,11 +14,9 @@
 	
 	var FeedbackTabView = Backbone.View.extend({
 		initialize: function(){
-			var cellphone=LocalModel.getUserPhoneNumber();
-			if(!cellphone)cellphone="";
 			var scroller = new Scroller();
 			this.scroller = scroller;
-			scroller.html(_.template(tabTemplate,{cellphone:cellphone}));
+			scroller.html(_.template(tabTemplate));
 			$(this.el).html(scroller.el);
 			$(this.el).css('display', '-webkit-box');	
 			$(this.el).css('-webkit-box-flex', '10');
@@ -30,19 +28,20 @@
 		render: function(){
 			this.scroller.render();
 			this.delegateEvents();
+			$('#cellphone', this.el).val(LocalModel.getUserPhoneNumber());
 			return this;
 	   	},
 	   	sendFeedback: function(){
 	   		var recommandation = $('#recommendation', this.el).val();
-	   		var opinion = $('#opinion', this.el).val();
+	   		var errorReport = $('#errorReport', this.el).val();
 	   		var data = {};
 	   		data.UUID = LocalModel.getUUID();
 	   		data.appName = "o2oist";
 	   		data.customerName = LocalModel.getUserName();
-	   		data.customerPhoneNumber = LocalModel.getUserPhoneNumber();
+	   		data.customerPhoneNumber = $('#cellphone', this.el).val();
 	   		data.feedbacks = {};
 	   		data.feedbacks.recommandation = recommandation;
-	   		data.feedbacks.opinion = opinion;
+	   		data.feedbacks.errorReport = errorReport;
 	   		var url = FeedbackServiceUrl+'?action=sendFeedback';
 	   		$.ajax({
 				type: 'POST',
@@ -63,11 +62,17 @@
 				    alert('謝謝您的寶貴意見');
 				}
 			});
+			//store phone number
+			var tel = LocalModel.getUserPhoneNumber();
+			if(!tel || tel.length!=10){
+				LocalModel.setUserPhoneNumber($('#cellphone', this.el).val());
+			}
 	   	}
 	});
 	
 	window.myapp = window.myapp || {};
 	window.myapp.FeedbackTabView = FeedbackTabView;
-})(	window.myapp.Widget.Scroller,
+})(	window.myapp.LocalModel,
+	window.myapp.Widget.Scroller,
 	window.myapp.Api.FeedbackServiceUrl,
 	window.myapp.LocalModel);
