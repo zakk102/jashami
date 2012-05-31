170d123a754128251a29238c7bef4e156093680a
何育誠 <zakk@o2oist.com>
2012-05-31 18:45:04 +0800

    merge

--- js/settings.js
+++ js/settings.js
@@ -17,7 +17,7 @@
 			if( h>19 || (h==19&&m>=30) ) return false; //19:30~24:00
 			return true;
 		},
-		'appVersion': 'ver2.2.023'
+		'appVersion': 'ver2.2.024'
 	};
 	
 	window.myapp.Api = window.myapp.Api || {};
